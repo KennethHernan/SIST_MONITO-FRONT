210cbb0857e6af7e6b445879af68942009f72a50
Jorge Ore <jore24@autonoma.edu.pe>
2022-12-04 23:07:22 -0500

    update

--- src/views/HomeAuditor.jsx
+++ src/views/HomeAuditor.jsx
@@ -1,6 +1,6 @@
 import "../style/homeAuditor.css";
 import { HeaderAuditor } from "../components/ui/HeaderAuditor";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import buscar from "../assets/buscar.png";
 import React, { useState, useEffect } from "react";
 import { useAgents } from "../hooks/useAgents";
@@ -9,7 +9,8 @@ import { dataUser } from "../store/storeUser";
 
 export const HomeAuditor = () => {
   const [user , setUser] = useAtom(dataUser);
-    const [dataAgents, setDataAgents] = useState([]);
+  const navigate = useNavigate();
+  const [dataAgents, setDataAgents] = useState([]);
   const { getAgents } = useAgents();
 
   useEffect(() => {
@@ -78,61 +79,38 @@ console.log(dataAgents)
       </div>
       <div className="content-table">
         <div className="tabe-info">
-          <table>
+        <table>
             <thead>
               <tr>
                 <td>#</td>
-                <td>AUDITOR </td>
-                <td>SURVEY ID</td>
+                <td>AGENTE</td>
                 <td>CAMPAÑA</td>
-                <td>STATE</td>
-                <td>SCORE</td>
-                <td>FECHA DE CREACIÓN</td>
+                <td>N° DE ENCUESTAS</td>
                 <td></td>
               </tr>
-              <tr>
-                <td>364242</td>
-                <td>5006900004ppr6RAAQ</td>
-                <td>75727525</td>
-                <td>Banco Estado</td>
-                <td>FIRMADO</td>
-                <td>80%</td>
-                <td>Sep 23 16:56, 2022</td>
-                <td>
-                  <Link to="/EvaluationAuditor">
-                    <button>Auditar</button>
-                  </Link>
-                </td>
-              </tr>
-              <tr>
-                <td>364242</td>
-                <td>5006900004ppr6RAAQ</td>
-                <td>75727525</td>
-                <td>Banco Estado</td>
-                <td>FIRMADO</td>
-                <td>80%</td>
-                <td>Sep 23 16:56, 2022</td>
-                <td>
-                  <Link to="/EvaluationAuditor">
-                    <button>Auditar</button>
-                  </Link>
-                </td>
-              </tr>
-              <tr>
-                <td>364242</td>
-                <td>5006900004ppr6RAAQ</td>
-                <td>75727525</td>
-                <td>Banco Estado</td>
-                <td>FIRMADO</td>
-                <td>80%</td>
-                <td>Sep 23 16:56, 2022</td>
-                <td>
-                  <Link to="/EvaluationAuditor">
-                    <button>Auditar</button>
-                  </Link>
-                </td>
-              </tr>
             </thead>
+            <tbody>
+              {dataAgents.map((agents) => {
+                return (
+                  <tr key={agents.idAgent}>
+                    <td>{agents.idAgent}</td>
+                    <td>{agents.fullname}</td>
+                    <td>{agents.user.role.rolName}</td>
+                    <td>{agents.user.surveys}</td>
+                    <td>
+                      <button
+                        onClick={() => {
+                          navigate(`/EvaluationAgente/${agents.idAgent}`);
+                        }}
+                        className="audit-button"
+                      >
+                        Ver encuentas
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
           </table>
         </div>
         <div className="content-tabla-foot">
